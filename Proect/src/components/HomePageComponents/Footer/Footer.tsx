b0f881637b2b648c18627c5e17f4568ab0413cae
nikki <nikki@debian.NN>
2026-04-08 11:37:38 +0300

    add components

---
+++ Proect/src/components/HomePageComponents/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+
+      <div className="marquee">
+        <div className="marquee-content">
+          🌸 BookingHotel • Created with React & TypeScript • 
+          日本の旅へようこそ • by Sovetov Roman & Panasenco Nichita
+        </div>
+      </div>
+
+    </footer>
+  );
+};
+
+export default Footer;
