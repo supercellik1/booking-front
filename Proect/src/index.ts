a54f4cc501f6be271f1338dd11c99dcacd98d1a6
nikki <nikki@debian.NN>
2026-04-08 10:40:41 +0300

    add pages

---
+++ Proect/src/index.ts
@@ -0,0 +1,4 @@
+export { default as HomePage } from "./pages/HomePage";
+export { default as AuthPage } from "./pages/AuthPage";
+export { default as HotelDetailsPage } from "./pages/HotelDetailsPage"; 
+export {default as AboutUsPage } from "./pages/AboutUsPage"
---
+++ Proect/src/pages/HotelDetailsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HotelDetailsComponent from '../components/HotelDetailsPage/HotelDetailsPage';
+
+const HotelDetailsPage: React.FC = () => {
+  return (
+    <div className="hotel-page-layout">
+       <HotelDetailsComponent />
+    </div>
+  );
+};
+
+export default HotelDetailsPage;
